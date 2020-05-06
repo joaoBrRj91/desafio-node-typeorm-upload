2a8533e47c2bbecbfa90a86a45811f8a68bc4ad3
João Nascimento <joao1991algoritmo@gmail.com>
2020-05-06 16:23:31 -0300

    Criação dos migrations

---
+++ src/database/migrations/1588791648899-CreateRelationshipCategoryToTransaction.ts
@@ -0,0 +1,37 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class CreateRelationshipCategoryToTransaction1588791648899
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'transactions',
+      new TableColumn({
+        name: 'category_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'transactions',
+      new TableForeignKey({
+        name: 'category_transaction',
+        columnNames: ['category_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'categories',
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('transactions', 'category_transaction');
+    await queryRunner.dropColumn('transactions', 'category_id');
+  }
+}
